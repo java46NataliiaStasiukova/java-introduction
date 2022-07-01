b3352de0c2049c0ea42d143bfb0a179018ba2fd3
Nataliia <nstasiukova@gmail.com>
2022-07-01 05:02:37 +0300

    Classwork-4

--- src/telran/numbers/SportLotoAppl.java
+++ src/telran/numbers/SportLotoAppl.java
@@ -1,7 +1,5 @@
 package telran.numbers;
-/**
- * 
- * @author nataliiastasiukova
+/*
  *application should print out 7 random numbers in the range [1-49]
  */
 public class SportLotoAppl {
@@ -11,22 +9,14 @@ public class SportLotoAppl {
 		int res[] = new int[7];
 		for(int i = 0; i < res.length; i++) {
 			int temp = 0;
-			int contains = 0;
-		while(contains <= 0) {
+			int contains = 1;
+		while(contains >= 0) {
 			temp = getRandomNumber(min, max);
-			contains = searchNumber(res, temp);
+			contains = ArrayInt.indexOf(res, temp);
 			}			
 			res[i] = temp;
 			System.out.print(res[i] + " ");
 		}
-	}
-	private static int searchNumber(int[] ar, int temp) {
-		for(int i = 0; i < ar.length; i++) {
-			if(ar[i] == temp) {
-				return -1;
-			}
-		}
-		return 1;
 	}
 	private static int getRandomNumber(int min, int max) {
 		return (int) (min + Math.random() * (max - min + 1));
