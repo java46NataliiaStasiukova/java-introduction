ba61aacbd3ea31c10d5555a02aa9c8b1351a9d7f
Nataliia <nstasiukova@gmail.com>
2022-07-01 06:18:01 +0300

    Classwork-5

---
+++ src/telran/numbers/TictactoeGame.java
@@ -0,0 +1,18 @@
+package telran.numbers;
+
+public class TictactoeGame {
+	/**
+	 * 
+	 * @param matrix: square matrix (nRows == nColumns; where nRows >= 3)
+	 * @param nRow: (number of row)
+	 * @param nCol: (number of column)
+	 * @param symb: (being filled symbol either 'x' or 'o')
+	 * @return: 0 - game isn't over; 1 - game is over with the winner's move; 
+	 * 2 - game is over with draw
+	 */
+	public static int tictactoeMove(char matrix[][], int nRow, int nCol, char symb) {
+		//TODO		
+		return -1;
+	}
+
+}
